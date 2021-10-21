75b343510dcb5bc5c28b671c2e08354a969a343d
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-21 08:01:55 -0600

    Add concatenate string section of code.

--- RollOfTheDiceListBox/RollOfTheDiceListBox.vb
+++ RollOfTheDiceListBox/RollOfTheDiceListBox.vb
@@ -11,7 +11,36 @@ Public Class RollOfTheDiceListBox
         DiceRollsListBox.Items.Add("Line 2")
     End Sub
 
+    Function RandomNumber() As Integer
+        Dim rndNumber1 As Single
+        Dim rndNumber2 As Single
+
+        'Create two random values from 1-6 and add them together.
+        Randomize()
+        rndNumber1 = Int((6 * Rnd()) + 1)
+        rndNumber2 = Int((6 * Rnd()) + 1)
+        Return CInt(rndNumber1 + rndNumber2 - 2)
+    End Function
+
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
+    End Sub
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim diceNumbers(10) As Integer
+        Dim header(10) As String
+
+        'Total the number of times that each combination is rolled.
+        For i = 0 To 1000
+            diceNumbers(RandomNumber()) += 1
+        Next
+
+        'Create a header with the proper amount of columns
+        Dim rowData As String
+        For i = 0 To 10
+            rowData &= CStr(i + 2)
+        Next
+        DiceRollsListBox.Items.Add(rowData)
+
     End Sub
 End Class
