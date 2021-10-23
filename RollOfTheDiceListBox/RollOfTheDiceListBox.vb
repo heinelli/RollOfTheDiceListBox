412f5e0093b2201ee0646cb0965c75eb16e996e8
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-23 08:47:11 -0600

    Organize code and add Github link.

--- RollOfTheDiceListBox/RollOfTheDiceListBox.vb
+++ RollOfTheDiceListBox/RollOfTheDiceListBox.vb
@@ -1,13 +1,36 @@
 ﻿'Elliot Heiner
 'RCET 0265
 'Roll of the Dice - List Box
-'
+'https://github.com/heinelli/RollOfTheDiceListBox.git
 
 Option Strict On
 Option Explicit On
 Public Class RollOfTheDiceListBox
-    Private Sub RollOfTheDiceListBox_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim diceNumbers(10) As Integer
+        Dim headerRow As String
+        Dim dataRow As String
 
+        'Total the number of times that each combination is rolled.
+        For i = 0 To 1000
+            diceNumbers(RandomNumber()) += 1
+        Next
+
+        'Create a header with the proper amount of columns.
+        DiceRollsListBox.Items.Add("Roll of the Dice".PadLeft(43))
+        DiceRollsListBox.Items.Add(StrDup(66, "-"))
+        For i = 0 To 10
+            headerRow &= CStr(i + 2).PadLeft(5) & "|"
+        Next
+        DiceRollsListBox.Items.Add(headerRow)
+        DiceRollsListBox.Items.Add(StrDup(66, "-"))
+
+        'Concatenate the total of each dice roll into one formatted line.
+        For i = 0 To 10
+            dataRow &= CStr(diceNumbers(i)).PadLeft(5) & "|"
+        Next
+        DiceRollsListBox.Items.Add(dataRow)
+        DiceRollsListBox.Items.Add(StrDup(66, "-"))
     End Sub
 
     Function RandomNumber() As Integer
@@ -22,31 +45,8 @@ Public Class RollOfTheDiceListBox
     End Function
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Close program
         Me.Close()
-    End Sub
-
-    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
-        Dim diceNumbers(10) As Integer
-        Dim rowData As String
-        Dim diceRollData As String
-
-        'Total the number of times that each combination is rolled.
-        For i = 0 To 1000
-            diceNumbers(RandomNumber()) += 1
-        Next
-
-        'Create a header with the proper amount of columns.
-        For i = 0 To 10
-            rowData &= CStr(i + 2).PadLeft(5) & "|"
-        Next
-        DiceRollsListBox.Items.Add(rowData)
-        DiceRollsListBox.Items.Add(StrDup(66, "-"))
-
-        'Concatenate the total of each dice roll into one formatted line.
-        For i = 0 To 10
-            diceRollData &= CStr(diceNumbers(i)).PadLeft(5) & "|"
-        Next
-        DiceRollsListBox.Items.Add(diceRollData)
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
